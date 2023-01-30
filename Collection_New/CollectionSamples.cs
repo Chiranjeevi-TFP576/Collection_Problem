af76a832541b3b8d1fa1cd58547ba32545bf5767
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-30 21:33:08 +0530

    [chiru] Added Collection_Programme

---
+++ Collection_New/CollectionSamples.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_New
+{
+    public class CollectionSamples
+    {
+        public void setDemo()
+        {
+            Console.WriteLine("\nIn SetDemo");
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Ravi");
+            set.Add("Vijay");
+            set.Add("Ravi");
+            set.Add(null);           
+            set.Add("Ajay");
+            HashSet<string>.Enumerator enumerator = set.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+           
+        }
+        public void doQueueDemo()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Amit Sharma");
+            queue.Enqueue("Vijay Raj");
+            queue.Enqueue("Jaishankar");
+            queue.Enqueue("Raj");
+            Console.WriteLine("Head:" + queue.Peek());
+            Console.WriteLine("\nIterating the queue elements: ");
+            foreach (var element in queue)
+            {
+                Console.WriteLine(element);
+            }
+            string dequeue = queue.Dequeue();
+            Console.WriteLine("Dequeue element: " + dequeue);
+            Console.WriteLine("\nIterating the queue elements after dequeu one element:");
+            Queue<string>.Enumerator enumerator = queue.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+
+        public void doDictionaryDemo()
+        {
+            Console.WriteLine("\nIn doDictionaryDemo");
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(100, "Amit");
+            dictionary.Add(101, "Rahul");
+            dictionary.Add(102, "Vijay");
+            Console.WriteLine("Access value using key(key=100): " + dictionary[100]);
+            Console.WriteLine("\nIterating Dictionary: ");
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine("key=" + element.Key + "& Value=" + element.Value);
+            }
+            Dictionary<int, string>.Enumerator obj = dictionary.GetEnumerator();
+            while (obj.MoveNext())
+            {
+                Console.WriteLine(obj.Current);
+            }
+        }
+        public  void doStackDemo()
+        {
+            Console.WriteLine("\nIn doStackDemo");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Ayush");
+            stack.Push("Garvit");
+            stack.Push("Amit");
+            stack.Push("Ashish");
+            stack.Push("Garina");
+            string pop = stack.Pop();
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("Poped element: " + pop);
+        }
+
+        public  void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDemo");
+            List<string> list = new List<string>();
+            list.Add("Ravi");
+            list.Add("Vijay");
+            list.Add("Ravi");
+            list.Add("Ajay");
+            //list.Add(123);
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("Accessing element of List using Index: " + list[3]);
+        }
+    }
+}
---
+++ Collection_New/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Collection_New
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Collection Problem");
+
+            CollectionSamples Collection = new CollectionSamples();
+            Collection.setDemo();
+            Collection.doListDemo();
+            Collection.doStackDemo();
+            Collection.doDictionaryDemo();
+            Collection.doQueueDemo();
+        }
+    }
+}
